989cc4b87f25a1bd77d74bcb781efa6440d29932
Li Gongheng <ligongheng@foxmail.com>
2019-10-12 17:16:25 +0800

    新增一个main的类

---
+++ src/main/java/Main.java
@@ -0,0 +1,10 @@
+import com.sun.xml.internal.ws.message.source.PayloadSourceMessage;
+
+/**
+ * Created by Li Gongheng on 2019/10/12.
+ */
+public class Main {
+    public static void main(String [] args ){
+        System.out.println("我的第一个git代码");
+    }
+}
